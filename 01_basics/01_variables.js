c334c18d37f1d465207961a8896aac8fe273a20c
Yesh raj <145488714+iamyashxraj@users.noreply.github.com>
2024-01-01 19:45:31 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,10 @@ const accountid = 12345
 let accountEmail = "iamyeshraj@outlook.com"
 var acountpassword = "12345"
 let accountState;
+accontcity = "Bengaluru"
 
 console.log(accountid);
-console.table([accountid,accountEmail,accountState,acountpassword]);
+console.table([accountid,accontcity,accountEmail,accountState,acountpassword]);
 /*
 perfer not to use var
 because of issue in block scope and functional scope
